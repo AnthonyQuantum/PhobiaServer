e98c800dded03e7d472cbb6bc8283bb310df43c3
AnthonyQuantum <quantumcreeper12@gmail.com>
2018-04-29 21:37:42 +0300

    Initial commit

---
+++ web.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var exec = require('child_process').exec;
+
+app.get("/", function(req, res) {
+var dl = 'curl ' + req.query.q + ' > image.jpg';
+    exec(dl, function(error, stdout, stderr) {
+        console.log(stderr);
+    });
+
+var cmd = 'python3 \
+classify_image.py \
+--image_file image.jpg';
+    exec(cmd, function(error, stdout, stderr) {
+        res.end(stdout);
+        console.log(stdout);
+    });
+});
+
+app.listen(process.env.PORT, function() {
+    console.log("server running on " + process.env.PORT);
+});
